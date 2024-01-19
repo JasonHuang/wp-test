4e34fe1b9ddf13f95f8cba36acb571343106f218
JasonHuang <huangpengxiao@gmail.com>
2024-01-19 01:10:01 -0500

    take down the strategy to fetch data from wordpress

--- wp-content/themes/techqik/functions.php
+++ wp-content/themes/techqik/functions.php
@@ -1,13 +1,4 @@
 <?php
-// add_action( 'after_setup_theme', 'theme_techqik_com_setup' );
-
-// function theme_techqik_com_setup() {
-//     add_theme_support( 'wp-block-styles' );
-// }
-
-
-// define( 'TECHQIK_THEME_DIR', trailingslashit( get_template_directory() ) );
-// require_once TECHQIK_THEME_DIR . 'inc/core/common-functions.php';
 
 /**
  * check if the techqik_enqueue_vue exists.
